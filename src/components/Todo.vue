1b6d40031cfdd6de8e0edcb5b943dda180ce5f1f
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2024-02-23 19:44:21 +0600

    ui add

---
+++ src/components/Todo.vue
@@ -0,0 +1,56 @@
+<script setup>
+import TodoItem from "./TodoItem.vue";
+
+
+</script>
+
+<template>
+  <div>
+    <div class="todo d-flex align-items-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-6">
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                  <button
+                    class="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Add Task
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  class="accordion-collapse collapse show"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body">
+                    <input class="form-control" type="text" />
+                    <button type="button" class="btn btn-primary mt-2">
+                      Add Task
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <TodoItem />
+          </div>
+          <div class="col-md-3"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.todo {
+  height: 100vh;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,36 @@
+<script></script>
+
+<template>
+  <div>
+    <h4 class="mt-5">Task List</h4>
+    <div class="d-flex bg-light justify-content-between align-items-center p-2">
+      <div>
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value=""
+          id="flexCheckDefault"
+        />
+        <label class="form-check-label ms-2" for="flexCheckIndeterminate">
+          Indeterminate checkbox
+        </label>
+      </div>
+      <div>
+        <button class="btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-three-dots-vertical"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
